a116902ad3ab43da9fd8304bcf8c7d0710ebc03b
selene-cho <selene.cho29@gmail.com>
2023-06-10 03:49:05 +0900

    feat: 헤더 - 검색 바 스크립트

--- js/main.js
+++ js/main.js
@@ -27,3 +27,21 @@ function hideBasket() {
 }
 
 /* HEADER - search (검색) */
+const headerEl = document.querySelector('header');
+const searchWrapEl = headerEl.querySelector('.search-wrap');
+const searchStarterEl = headerEl.querySelector('.search-starter');
+const searchCloserEl = searchWrapEl.querySelector('.search-closer');
+const searchShadowEl = searchWrapEl.querySelector('.shadow');
+
+searchStarterEl.addEventListener('click', showSearch);
+searchCloserEl.addEventListener('click', hideSearch);
+searchShadowEl.addEventListener('click', hideSearch);
+
+function showSearch() {
+  headerEl.classList.add('searching');
+  document.documentElement.classList.add('fixed'); // documnetElement : document의 최상위 element를 뜻함 = html
+}
+function hideSearch() {
+  headerEl.classList.remove('searching');
+  document.documentElement.classList.remove('fixed');
+}
