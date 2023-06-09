de629d9cb5342d5a3af196aedfc5b89e7919ba03
selene-cho <selene.cho29@gmail.com>
2023-06-10 03:09:07 +0900

    feat: 헤더 - 장바구니 드롭다운 메뉴 스크립트

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const basketStarterEl = document.querySelector('header .basket-starter');
+const basketEl = basketStarterEl.querySelector('.basket');
+
+basketStarterEl.addEventListener('click', function (event) {
+  event.stopPropagation();
+  if (basketEl.classList.contains('show')) {
+    // contains: 해당 요소에 특정한 클래스값 존재하는지 여부 (true/false 값 반환)
+    hideBasket();
+  } else {
+    showBasket();
+  }
+});
+basketEl.addEventListener('click', function (event) {
+  event.stopPropagation();
+});
+
+window.addEventListener('click', function () {
+  hideBasket();
+});
+
+function showBasket() {
+  basketEl.classList.add('show');
+}
+function hideBasket() {
+  basketEl.classList.remove('show');
+}
