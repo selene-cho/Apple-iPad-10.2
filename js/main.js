adb5dc85c57160fc0dd56325e0f4999e94ac320c
selene-cho <selene.cho29@gmail.com>
2023-06-11 21:52:36 +0900

    feat: 헤더 - 검색 바 애니메이션

--- js/main.js
+++ js/main.js
@@ -28,10 +28,12 @@ function hideBasket() {
 
 /* HEADER - search (검색) */
 const headerEl = document.querySelector('header');
+const headerMenuEls = [...headerEl.querySelectorAll('ul.menu > li')];
 const searchWrapEl = headerEl.querySelector('.search-wrap');
 const searchStarterEl = headerEl.querySelector('.search-starter');
 const searchCloserEl = searchWrapEl.querySelector('.search-closer');
 const searchShadowEl = searchWrapEl.querySelector('.shadow');
+const searchDelayEls = [...searchWrapEl.querySelectorAll('li')];
 
 searchStarterEl.addEventListener('click', showSearch);
 searchCloserEl.addEventListener('click', hideSearch);
@@ -39,9 +41,22 @@ searchShadowEl.addEventListener('click', hideSearch);
 
 function showSearch() {
   headerEl.classList.add('searching');
-  document.documentElement.classList.add('fixed'); // documnetElement : document의 최상위 element를 뜻함 = html
+  document.documentElement.classList.add('fixed'); // documentElement : document의 최상위 element를 뜻함 = html
+  headerMenuEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = (index * 0.4) / headerMenuEls.length + 's';
+  });
+  searchDelayEls.forEach(function (el, index) {
+    el.style.transitionDelay = (index * 0.4) / searchDelayEls.length + 's';
+  });
 }
 function hideSearch() {
   headerEl.classList.remove('searching');
   document.documentElement.classList.remove('fixed');
+  headerMenuEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = (index * 0.4) / headerMenuEls.length + 's';
+  });
+  searchDelayEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = (index * 0.4) / searchDelayEls.length + 's';
+  });
+  searchDelayEls.reverse(); /* 다시 검색 열었을때 원래 순서로 나오게 하기위해 다시 원래대로 돌림 */
 }
